f2a77c92b6124d5aa5bcd4b2f3b7cfaa5709a4a6
John San Pietro <msanpietro@gmail.com>
2021-03-23 18:31:37 -0400

    Read method

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,3 +9,16 @@ export const createPost = async (req, res) => {
      res.status(400).json({ success: false, error });
     }
    }
+
+   await post.save();
+
+   res.status(201).json(post);
+
+   export const getPosts = async (req, res) => {
+    try {
+     const posts = await Post.find();
+     res.status(200).json(posts);
+    } catch (error) {
+     res.status(400).json({ success: false, error });
+    }
+   }
