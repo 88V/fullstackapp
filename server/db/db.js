9fe6f3f8c6089f63bb4c6eac8eff51d61c0b176c
John San Pietro <msanpietro@gmail.com>
2021-03-23 18:28:13 -0400

    project files and added mongo db connection string

---
+++ server/db/db.js
@@ -0,0 +1,2 @@
+import mongoose from 'mongoose';
+mongoose.connect('mongodb+srv://Johnsp:Password123@cluster0.xr1ou.mongodb.net/johndb?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
